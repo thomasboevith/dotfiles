46294c9e93ce8aeeb420c2a851d266ceb623b2f2
Thomas Bøvith <thomasboevith@users.noreply.github.com>
2019-05-20 15:58:18 +0200

    Update refreshdotfiles.sh

--- refreshdotfiles.sh
+++ refreshdotfiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget http://github.com/thomasboevith/dotfiles/raw/master/dotemacs -O ~/.emacs
-wget http://github.com/thomasboevith/dotfiles/raw/master/dotbashrc -O ~/.bashrc
+wget http://github.com/thomasboevith/dotfiles/raw/master/dotemacs -O - >| ~/.emacs
+wget http://github.com/thomasboevith/dotfiles/raw/master/dotbashrc -O - >| ~/.bashrc
