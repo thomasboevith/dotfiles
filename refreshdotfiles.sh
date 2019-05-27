55e6dc4f062d5310e9dc6f2a8c00738ce0e0d6b1
Thomas Bøvith <thomasboevith@users.noreply.github.com>
2019-05-27 17:08:15 +0200

    Update refreshdotfiles.sh

--- refreshdotfiles.sh
+++ refreshdotfiles.sh
@@ -2,4 +2,4 @@
 wget http://github.com/thomasboevith/dotfiles/raw/master/dotemacs -O - >| ~/.emacs
 wget http://github.com/thomasboevith/dotfiles/raw/master/dotbashrc -O - >| ~/.bashrc
 wget http://github.com/thomasboevith/dotfiles/raw/master/dotscreenrc -O - >| ~/.screenrc
-wget http://github.com/thomasboevith/dotfiles/raw/master/abcdedotconf -O - >| ~/abcde.conf
+wget http://github.com/thomasboevith/dotfiles/raw/master/abcdedotconf -O - >| ~/.abcde.conf
